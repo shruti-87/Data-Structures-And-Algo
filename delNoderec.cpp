72d2b23c3568560c0b2bfefba3f7931c17664337
shruti-87 <100983511+shruti-87@users.noreply.github.com>
2022-04-13 12:56:37 +0530

    Add files via upload

---
+++ delNoderec.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+Node *takeInput(){
+    int data;
+    cin>>data;
+    Node *head = NULL, *tail = NULL;
+    
+    while()    
+}
